693da197de5134c4d4fe0cce2aec53ad85eda2b6
gksmfahd78 <54091411+gksmfahd78@users.noreply.github.com>
2020-06-06 13:29:14 +0900

    기본셋팅

---
+++ app/src/main/java/com/dt/lattedb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dt.lattedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
